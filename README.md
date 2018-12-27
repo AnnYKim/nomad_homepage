609fdea6db714b2e1fb0bfd57bab18b716934c72
nykim <nykim@nykim.net>
2018-12-27 17:37:35 +0900

    remove unnecessary comments

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # [ Nomad Connection Homepage ]
 
-**최종 수정일**: 2018-12-26
+**최종 수정일**: 2018-12-27
 
 ## 폴더 구조
 
@@ -20,7 +20,7 @@
 - index.html
 - README.md
 
-## 전달사항
+## CSS
 
 - 풀 반응형이며, 최소 사이즈 720부터 대응합니다. (갤럭시 S5 크기)
   - 반응형 분기는 아래와 같습니다.
@@ -31,4 +31,29 @@
     @include res--mobile {color:blue;} //모바일 사이즈용
     ```
 - 폰트는 경량화 폰트를 사용했습니다. 따라서 일부 특수문자는 출력되지 않을 수 있습니다.
+
   - 사용하는 폰트 굵기: Thin(100), Light(200), DemiLight(300), Regular(400), Medium(500), Bold(700)
+
+- 컴파일된 css 파일은 css/ 폴더 내 style.css입니다. css를 수정하시려면 scss/ 폴더 내 scss 파일을 수정하시고 컴파일해주세요.
+  - 만약 scss 파일 없이 작업하시려면 css/ 폴더에 압축되지 않은 style-not-compressed.css 파일이 있으니, 이를 수정하시고 해당 파일로 스타일을 연결하시면 됩니다.
+
+## 전달사항
+
+- 파일 다운로드 버튼을 누르면 현재 alert창이 나옵니다. 이를 수정하시려면 아래와 같이 해주세요!
+
+1. files/ 폴더에 파일을 업로드해주세요.
+   > 예를 들어, files/ 내에 introduction.pdf를 업로드 합니다.
+2. js/ 폴더의 ui.js 파일을 여신 다음, 8번째 줄의 fileName 변수를 찾습니다.
+
+```
+  var fileName = "";
+```
+
+3. 위 fileName의 "" 안에 확장자를 포함한 파일이름 전체를 넣어주세요.
+
+```
+  var fileName = "introduction.pdf";
+```
+
+- 회사 주소 등 텍스트를 수정하려는 경우, index.html 내에 해당 텍스트를 직접 수정하시면 됩니다.
+  - 단, 영문 타이틀(Our Value 등)은 이미지로 되어 있으니, 이미지를 통째로 교체하셔야 합니다. (이 경우 css내의 width, height값도 함께 변경하셔야 깨지지 않습니다!)
--- js/ui.js
+++ js/ui.js
@@ -1,13 +1,17 @@
 var header = document.querySelector(".header");
 var burger = header.querySelector(".burger");
 var nav = header.querySelector(".nav");
-var sectionTop = []; //각 section의 위치
-var headerHeight = 64; //header의 높이
+var sectionTop = [];
+var headerHeight = 64;
 
+// ------------------
+var fileName = "";
+// 다운로드 파일네임 ("" 안에 확장자를 포함한 파일이름 전체를 넣어주세요)
+// 예) var fileName = "introduction.pdf";
+// ------------------
 
-
-
-function getScrollBarWidth() { //스크롤바 구하기
+// 스크롤바 구하기
+var getScrollBarWidth = function () {
   document.body.style.overflow = 'hidden';
   var width = document.body.clientWidth;
   document.body.style.overflow = 'scroll';
@@ -17,7 +21,7 @@ function getScrollBarWidth() { //스크롤바 구하기
   return width;
 }
 
-
+// 스크롤 방지
 var $preventScroll = function () {
   $("body").css({
     "margin-right": getScrollBarWidth(),
@@ -25,11 +29,12 @@ var $preventScroll = function () {
   }).addClass("no-scroll");
 }
 
+// 스크롤 허용
 var $allowScroll = function () {
   $("body").removeAttr("style").removeClass("no-scroll");
 }
 
-
+// 메뉴 열기 (모바일)
 var openMenu = function () {
   header.classList.add("active");
   setTimeout(function () {
@@ -40,6 +45,7 @@ var openMenu = function () {
 
 };
 
+// 메뉴 닫기 (모바일)
 var closeMenu = function () {
   nav.classList.remove("active");
   setTimeout(function () {
@@ -58,14 +64,15 @@ var handleBugerClick = function () {
   }
 };
 
-//각 section의 위치 구하는 함수
+// 각 section의 위치 구하기
 var $getSectionTop = function () {
   $(".section-to-scroll").each(function (idx) {
     sectionTop[idx] = Math.ceil($(this).offset().top);
   });
 };
 
-var $pageScroll = function (where, anim) {
+// 스크롤 애니메이션
+var $pageScroll = function (where) {
   var $body = $("html, body");
   $body.stop().animate({
       scrollTop: where
@@ -74,28 +81,31 @@ var $pageScroll = function (where, anim) {
   );
 };
 
+// 로고 클릭 시
 var logoClick = function (e) {
   if (e.target.className === "logo") {
     $pageScroll(0);
   }
 }
 
+// 헤더 높이값 구하기
 var getHeaderHeight = function () {
   headerHeight = header.offsetHeight;
 };
 
+// 네비 높이 강제 지정 (모바일)
 var setNavHeight = function () {
   var navContainer = header.querySelector(".nav-container");
   var windowHeight = window.innerHeight;
-  navContainer.style.height = windowHeight - 64 - 145 + "px";
+  navContainer.style.height = windowHeight - 209 + "px";
 };
 
+// 헤더 메뉴 클릭 시 해당 위치로 이동
 var $headerMenuClick = function () {
   $(".nav a").on('click', function (e) {
     e.preventDefault();
     var idx = $(".nav a").index($(this));
     var position = sectionTop[idx];
-    // var position = sectionTop[idx] - headerHeight;
     nav.classList.remove("active");
     header.classList.remove("active");
     $pageScroll(position);
@@ -103,6 +113,7 @@ var $headerMenuClick = function () {
   });
 };
 
+// 윈도 너비 체크
 var checkScreenSize = function () {
   var windowWidth = window.innerWidth;
   if (windowWidth > 769) {
@@ -113,31 +124,33 @@ var checkScreenSize = function () {
   }
 };
 
+// 파일 다운로드
 var fileDownload = function (link) {
   var newWindow = window.open("about:blank");
   newWindow.location.href = link;
 };
 
+// 다운로드 버튼 클릭
 var downloadIntroduction = function () {
   const downloadButton = document.querySelector(".button-download");
   downloadButton.addEventListener("click", function () {
-    // fileDownload("files/test.pdf");
-    alert("파일 준비 중!")
+    (fileName == "") ? alert("회사소개서를 준비 중입니다 :)"): fileDownload("files/" + fileName);
   });
 };
 
-var handleWindowReszie = function () {
+var handleWindowResize = function () {
   $getSectionTop();
   getHeaderHeight();
   checkScreenSize();
+  setNavHeight();
 };
 
 var init = function () {
   $getSectionTop();
   $headerMenuClick();
   downloadIntroduction();
-  window.addEventListener("load", handleWindowReszie);
-  window.addEventListener("resize", handleWindowReszie);
+  window.addEventListener("load", handleWindowResize);
+  window.addEventListener("resize", handleWindowResize);
   burger.addEventListener("click", handleBugerClick);
   header.addEventListener("click", logoClick);
 };
